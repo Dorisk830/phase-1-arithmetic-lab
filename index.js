e0d2d68ae60e678287ce5e73e4bcc3dd397f3514
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-10-06 07:31:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
+// index.js
+function findNumbersForProduct(targetProduct) {
+    for (let num1 = 1; num1 <= targetProduct; num1++) {
+        for (let num2 = 1; num2 <= targetProduct; num2++) {
+            if (num1 * num2 === targetProduct) {
+                return [num1, num2];
+            }
+        }
+    }
+    return null; // No such pair found
+}
 
-// Write your code here
+const targetProduct = 62;
+const [num1, num2] = findNumbersForProduct(targetProduct);
+const multiply = num1 * num2;
+
+function getRandomIntegerGreaterThanZero() {
+    return Math.floor(Math.random() * 100) + 1; // Generates a random integer between 1 and 100
+}
+
+const random = getRandomIntegerGreaterThanZero()
+
+
+// mod.js (your implementation)
+function calculateMod(num3, num4) {
+    return num3 % num4;
+}
+const num3 = 10;
+const num4 = 6;
+const mod = calculateMod(num3, num4)
+
+// max.js (your implementation)
+function findMaxNumber(numbers) {
+    return Math.max(...numbers);
+}
+
+
+const numbers = [10, 5, 20, 15, 8];
+
+const max = findMaxNumber(numbers);
+
+
+
+
+
